76dfd01afb30a52d521ff64c2e0a6e4849fa48ce
xitraya-lisica <xitraya_lisica@ukr.net>
2025-02-16 20:21:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-
-
-
 dotenv.config();
 const app = express();
 const port = 3000;
